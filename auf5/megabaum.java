48446d352a3c510df07c3a7ba87c428e207f666f
NicoNsta <nicolas.huhle765@gmail.com>
2023-05-02 19:58:51 +0200

    we done bro

--- auf5/megabaum.java
+++ auf5/megabaum.java
@@ -1,27 +1,27 @@
 package auf5;
+
 public class megabaum  {
 
     private static int counter = 0;
     final static int omga = 30; // 5 - 79
     final static double pRadius = .006;
-    final static double ax = 960;
-    final static double laenge = 100;
-    final static double mLaenge = laenge;
+    final static double ax1 = 400;
+    final static double ax2 = 1400;
+    final static double laenge1 = 100;
+    final static double laenge2 = 60;
     final static double gamma = 0;
-    final static double hoehe = Math.random();
-    final static boolean green = false;
+    final static boolean green = true;
 
     public static void main(String[] args) {
+        StdDraw.setPenColor(StdDraw.BROWN);
         StdDraw.setCanvasSize(1920, 1080);
         StdDraw.setXscale(0, 1920);
         StdDraw.setYscale(0, 1080);
-//        StdDraw.show(0);
-        StdDraw.setPenColor(StdDraw.BROWN);
-//        tree1(gamma, laenge, ax, 150);
         StdDraw.show(0);
-        tree2(gamma, laenge, ax, 150, hoehe);
+        tree1(gamma, laenge1, ax1, 100);
         StdDraw.show(0);
-
+        tree2(gamma, laenge2, ax2, 100);
+        StdDraw.show(0);
     }
     public static void tree1(double gamma, double laenge, double ax, double ay) {
         if (laenge < 1){
@@ -63,33 +63,31 @@ public class megabaum  {
         StdDraw.line(BX, BY, CX, CY);
         StdDraw.line(CX, CY, EX, EY);
 
-            tree1(gamma + omga, u, DX, DY);
-            tree1(gamma-(90 - omga), v, EX, EY);
+        tree1(gamma + omga, u, DX, DY);
+        tree1(gamma-(90 - omga), v, EX, EY);
     }
 
-    public static void tree2(double gamma, double laenge, double ax, double ay, double hoehe) {
+    public static void tree2(double gamma, double laenge, double ax, double ay) {
         if (laenge < 1){
             return;
         }
 
-        double rgamma;
 
-        if(laenge == 100){
-            rgamma = gamma;
-        } else {
-            rgamma = gamma + Math.random() * 180 - 90;
-        }
+        double minOmga = 0;
+        double maxOmga = 90;
+        double rdmOmga = minOmga + (Math.random() * (maxOmga - minOmga));
 
-        double winkl = Math.toRadians(rgamma);
+
+        double winkl = Math.toRadians(gamma);
         System.out.println("winkl: " + winkl);
-        double omega = Math.toRadians(omga);
+        double omega = Math.toRadians(rdmOmga);
 
         double s = laenge * Math.sin(winkl);
         double c = laenge * Math.cos(winkl);
         double u = laenge * Math.cos(omega);
         double v = laenge * Math.sin(omega);
 
-        double r = Math.random() * 4 + 1;
+        double r = Math.random() * 2 + 1;
 
         double BX = ax + c;
         double BY = ay + s;
@@ -115,13 +113,10 @@ public class megabaum  {
 
         StdDraw.line(ax, ay, DX, DY);
         StdDraw.line(BX, BY, CX, CY);
-        StdDraw.line(CX, CY, EX, EY);
 
-        tree2(rgamma + omga, u, DX, DY, hoehe);
-        tree2(rgamma - (90 - omga), v, EX, EY, hoehe);
-
+        tree2(gamma + rdmOmga, u, DX, DY);
+        tree2(gamma - (90 - rdmOmga), v, EX, EY);
     }
-
 
     private static java.awt.Color getColor() {
         counter++;
